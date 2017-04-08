6108f95b3560712adbfe774df0bce2705d2770a5
Nick Ivanov <nick_ivanov@list.ru>
2017-04-08 22:03:51 +0300

    VersionAdded

---
+++ src/BoxOptionsApi/Controllers/VersionController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BoxOptionsApi.Controllers
+{
+    [Route("home/[controller]")]
+    public class VersionController : Controller
+    {
+        [HttpGet]
+        public VersionModel Get()
+        {
+            return new VersionModel
+            {
+                Version = Microsoft.Extensions.PlatformAbstractions.PlatformServices.Default.Application.ApplicationVersion
+            };
+        }
+
+        public class VersionModel
+        {
+            public string Version { get; set; }
+        }
+    }
+}
